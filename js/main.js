b8c880d2a6543e716668771ea73656626a537a90
Rano Hardjosemito <r.hardjo@live.nl>
2017-01-20 18:22:23 +0100

    Created frontpage & functional file upload script.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+    $(".btn").click(function(e){
+        dataModal = $(this).attr("data-modal");
+        $("#" + dataModal).css({"display":"block"});
+    });
+
+    $(".close-modal, .modal-sandbox").click(function(){
+        $(".modal").css({"display":"none"});
+    });
+    
+});
